cf9f3d8de2c79f4d52e5572c872329dcc98a859d
alex s <sanatateacom@gmail.com>
2021-05-22 10:55:11 +0300

    created history for post

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -14,6 +14,18 @@
                         @method('put')
                         @include('admin.posts._form', ['post' => $post])
                     </form>
+
+                    @admin
+                        <div class="mt-4">
+                            <hr>
+                            @forelse($post->history as $item)
+                                @dd($item)
+                                <div>{{ $item->email }} - {{ $item->pivot->created_at->diffForHumans() }} - {{ $item->pivot->before }} - {{ $item->pivot->after }}</div>
+                            @empty
+                                <div>No history</div>
+                            @endforelse
+                        </div>
+                    @endadmin
                 </div>
             </div>
         </div>
