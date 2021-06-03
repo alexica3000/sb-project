a89eaab71ef784f7f98c03a47040e85fd664e223
alex s <sanatateacom@gmail.com>
2021-06-03 21:31:57 +0300

    fixed comment service

--- app/Http/Services/CommentsService.php
+++ app/Http/Services/CommentsService.php
@@ -3,11 +3,11 @@
 namespace App\Http\Services;
 
 use App\Http\Requests\Comments\StoreCommentsRequest;
-use App\Models\Interfaces\CommentsInterface;
+use App\Models\Interfaces\HasCommentsInterface;
 
 class CommentsService
 {
-    public function store(StoreCommentsRequest $request, CommentsInterface $resource)
+    public function store(StoreCommentsRequest $request, HasCommentsInterface $resource)
     {
         $resource->comments()->create($request->validated() + ['user_id' => auth()->id()]);
     }
